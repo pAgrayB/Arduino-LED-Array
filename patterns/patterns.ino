fd58045720cc8eeff497f8893ba49c312f8bfd0e
pAgrayB <ptgraybeal22.vt.edu>
2019-06-10 15:58:49 -0400

    Compiled all patterns into a single file.

--- patterns/patterns.ino
+++ patterns/patterns.ino
@@ -6,7 +6,7 @@
 #define first 0b1000001110000011
 #define second 0b1100000111000001
 
-typedef enum Patterns {train, love, party, MAX_PATS} pattern_t;
+typedef enum Patterns {train, love, woop, bounce, MAX_PATS} pattern_t;
 
 
 int latchPin = 8;
@@ -19,8 +19,18 @@ uint16_t value = 0b1110000011100000;
 uint16_t woopValue = 0b0111100000000000;
 char op = 'r';
 
-pattern_t current = train;
+// Global variables for "bounce" pattern
+uint16_t value2 = 1;
+bool op2 = true; // Operation to perform on value
+float h = 2; // Height between LEDs (m)
+float vel = 0; // Initial velocity is 0 (m/s)
+float time = sqrt((2 * h)/9.81); // Time between latches (sec)
+// End variables for "bounce" pattern
 
+
+// SELECT PATTERN HERE
+pattern_t current = bounce;
+// SELECT PATTERN HERE
 
 
 void setup() {
@@ -42,10 +52,13 @@ void loop() {
       bitterSweetPattern();
       break;
 
-    case party:
-      woop();
+    case woop:
+      woopPattern();
       break;
-      
+
+    case bounce:
+      bouncePattern();
+      break;
 
     default:
       turnOnMany(0b1001100110011001);
@@ -58,7 +71,50 @@ void loop() {
 // Runs each pattern function in the main
 // loop of the arduino ALONE for the effect
 
-void woop () {
+void bouncePattern () {
+
+    // take the (SS) latchPin low so 
+    // the LEDs don't change while you're sending in bits:
+    digitalWrite(latchPin, LOW);
+    
+    // shift out the bits:
+    SPI.transfer16(value2);
+
+    //take the (SS) latch pin high so the LEDs will light up:
+    digitalWrite(latchPin, HIGH);
+    // pause before next value:
+    delay(time * 1000); // delay needs msecs  
+
+    updateMovement();
+    updateDirection();
+}
+
+void updateMovement() {
+
+    if (op2) { // Falling
+      value2 *= 2; // shift one LED left
+      vel = vel - (9.81 * time); //update velocity
+      time = ( vel + sqrt(pow(vel,2) + 19.62 * h) ) / 9.81; //update LED timing
+    } else { // Rising
+      value2 /= 2; // shift one LED right
+      vel = vel - (9.81 * time); //update velocity
+      time = ( -1 * vel + sqrt(pow(vel,2) - 19.62 * h) ) / (-9.81); //update LED timing
+    }
+}
+
+void updateDirection() {
+
+      if (value2 == 32768) { // Left-most LED is reached
+      op2 = false; // start rising
+      vel = -1 * vel; // switch velocity direction for calculations
+    } else if (value2 == 1){ // Right-most LED is reached
+      op2 = true; // start falling
+      vel = 0; // reset velocity to 0 (avoids rounding errors)
+      time = sqrt((2 * h)/9.81); // reset timing to initially calculated (avoid rounding errors)
+    }
+}
+
+void woopPattern () {
 
   turnOnMany(woopValue);
   op = dir(woopValue, op);
